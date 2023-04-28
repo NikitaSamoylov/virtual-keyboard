421f50ac2682661aea77e28cbc1db6416feda12d
Nikita Samoylov <stealeagle87@gmail.com>
2023-04-28 20:28:11 +0300

    feat: add caps and shift letter case

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const textarea = document.querySelector('.textarea');
 const buttons = document.querySelectorAll('.button');
+const capsLock = document.querySelector('.capslock');
+const shift = document.querySelector('.shift');
+
+let isCapsLock = false;
+capsLock.addEventListener('click', () => {
+    if (!isCapsLock) {
+        isCapsLock = true
+        capsLock.style.color = 'orange'
+    } else {
+        isCapsLock = false
+        capsLock.style.color = '#e1dede'
+    };
+});
+
+let isShift = false;
+shift.addEventListener('click', () => {
+    if (!isShift) {
+        isShift = true;
+        shift.style.color = 'orange'
+    } else {
+        isShift = false
+        shift.style.color = '#e1dede'
+    }
+})
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -10,7 +34,15 @@ buttons.forEach((button) => {
             textarea.textContent += ' '
         } 
         else if (button.classList.contains('symbol-btn')) {
-            textarea.textContent += button.textContent
+            if (isCapsLock) {
+                textarea.textContent += button.textContent.toUpperCase()
+            }
+            else if (isShift) {
+                textarea.textContent += button.textContent.toUpperCase()
+            }
+            else {
+                textarea.textContent += button.textContent
+            }
         }
     });
 });
