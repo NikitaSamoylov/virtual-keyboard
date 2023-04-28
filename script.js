8be6ae1dd60ee61aade27d8a214c55d7603b1fde
Nikita Samoylov <stealeagle87@gmail.com>
2023-04-28 21:33:36 +0300

    feat: change symbols while the shift is true

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const textarea = document.querySelector('.textarea');
 const buttons = document.querySelectorAll('.button');
 const capsLock = document.querySelector('.capslock');
-const shift = document.querySelector('.shift');
+const shifts = document.querySelectorAll('.shift');
 
 let isCapsLock = false;
 capsLock.addEventListener('click', () => {
@@ -14,19 +14,177 @@ capsLock.addEventListener('click', () => {
     };
 });
 
+const changeBtnsValue = () => {
+    buttons.forEach((button) => {
+        if (button.classList.contains('tilda')) {
+            if (isShift) {
+                button.textContent = '~'
+            } else {
+                button.textContent = '`'
+            }
+        }
+        else if (button.classList.contains('one')) {
+            if (isShift) {
+                button.textContent = '!'
+            } else {
+                button.textContent = '1'
+            }
+        }
+        else if (button.classList.contains('two')) {
+            if (isShift) {
+                button.textContent = '@'
+            } else {
+                button.textContent = '2'
+            }
+        }
+        else if (button.classList.contains('three')) {
+            if (isShift) {
+                button.textContent = '#'
+            } else {
+                button.textContent = '3'
+            }
+        }
+        else if (button.classList.contains('four')) {
+            if (isShift) {
+                button.textContent = '$'
+            } else {
+                button.textContent = '4'
+            }
+        }
+        else if (button.classList.contains('five')) {
+            if (isShift) {
+                button.textContent = '%'
+            } else {
+                button.textContent = '5'
+            }
+        }
+        else if (button.classList.contains('six')) {
+            if (isShift) {
+                button.textContent = '^'
+            } else {
+                button.textContent = '6'
+            }
+        }
+        else if (button.classList.contains('seven')) {
+            if (isShift) {
+                button.textContent = '&'
+            } else {
+                button.textContent = '7'
+            }
+        }
+        else if (button.classList.contains('eight')) {
+            if (isShift) {
+                button.textContent = '*'
+            } else {
+                button.textContent = '8'
+            }
+        }
+        else if (button.classList.contains('nine')) {
+            if (isShift) {
+                button.textContent = '('
+            } else {
+                button.textContent = '9'
+            }
+        }
+        else if (button.classList.contains('zero')) {
+            if (isShift) {
+                button.textContent = ')'
+            } else {
+                button.textContent = '0'
+            }
+        }
+        else if (button.classList.contains('minus')) {
+            if (isShift) {
+                button.textContent = '_'
+            } else {
+                button.textContent = '-'
+            }
+        }
+        else if (button.classList.contains('equals')) {
+            if (isShift) {
+                button.textContent = '+'
+            } else {
+                button.textContent = '='
+            }
+        }
+        else if (button.classList.contains('open-bracket')) {
+            if (isShift) {
+                button.textContent = '{'
+            } else {
+                button.textContent = '['
+            }
+        }
+        else if (button.classList.contains('close-bracket')) {
+            if (isShift) {
+                button.textContent = '}'
+            } else {
+                button.textContent = ']'
+            }
+        }
+        else if (button.classList.contains('reverse-slash')) {
+            if (isShift) {
+                button.textContent = '|'
+            } else {
+                button.textContent = '\\'
+            }
+        }
+        else if (button.classList.contains('semicolon')) {
+            if (isShift) {
+                button.textContent = ':'
+            } else {
+                button.textContent = ';'
+            }
+        }
+        else if (button.classList.contains('apostrof')) {
+            if (isShift) {
+                button.textContent = '"'
+            } else {
+                button.textContent = '\''
+            }
+        }
+        else if (button.classList.contains('comma')) {
+            if (isShift) {
+                button.textContent = '<'
+            } else {
+                button.textContent = ','
+            }
+        }
+        else if (button.classList.contains('point')) {
+            if (isShift) {
+                button.textContent = '>'
+            } else {
+                button.textContent = '.'
+            }
+        }
+        else if (button.classList.contains('slash')) {
+            if (isShift) {
+                button.textContent = '?'
+            } else {
+                button.textContent = '/'
+            }
+        }
+    })
+}
+
 let isShift = false;
-shift.addEventListener('click', () => {
-    if (!isShift) {
-        isShift = true;
-        shift.style.color = 'orange'
-    } else {
-        isShift = false
-        shift.style.color = '#e1dede'
-    }
+shifts.forEach((shift) => {
+    shift.addEventListener('click', () => {
+        if (!isShift) {
+            isShift = true;
+            shift.style.color = 'orange'
+            changeBtnsValue()
+        } else {
+            isShift = false
+            shift.style.color = '#e1dede'
+            // returnInitBtnsValue()
+            changeBtnsValue()
+        }
+    })
 })
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
+
         if (button.classList.contains('backspace')) {
             textarea.textContent = textarea.textContent.slice(0, -1)
         }
